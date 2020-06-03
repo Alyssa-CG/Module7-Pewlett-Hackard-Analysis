dc784e51c0fcdf203819ba782b0462dfe1dcc91c
Alyssa Gomes <alyssa@Alyssas-MacBook-Air.local>
2020-06-03 17:17:31 -0400

    Challenge Table 1

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -203,3 +203,86 @@ FROM retirement_info as ri
 		ON (ri.emp_no = de.emp_no)
 WHERE (de.to_date = '9999-01-01')
 AND de.dept_no IN ('d005', 'd007');
+
+--
+
+-- CHALLENGE Table 1: Number of Retiring Employees by Title
+
+SELECT * FROM current_emp
+SELECT * FROM titles
+SELECT * FROM salaries
+
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	t.title,	
+	t.from_date,
+	s.salary
+INTO retiring_titles
+FROM current_emp as ce
+	INNER JOIN titles AS t
+		ON (ce.emp_no = t.emp_no)
+	INNER JOIN salaries AS s
+		ON (ce.emp_no = s.emp_no);
+
+-- DUPLICATES:
+-- Find duplicated names (by emp_no, first_name, last_name)
+-- Returns count. (Here returns 20,922 duplicated employees.) 
+SELECT
+  emp_no,
+  first_name,
+  last_name,
+  count(*)
+FROM retiring_titles
+GROUP BY
+  emp_no,
+  first_name,
+  last_name
+HAVING count(*) > 1;
+
+-- Display duplicate rows (42,526 rows)
+SELECT * FROM
+  (SELECT *, count(*)
+  OVER
+    (PARTITION BY
+     emp_no, 
+	 first_name,
+     last_name
+    ) AS count
+  FROM retiring_titles) tableWithCount
+  WHERE tableWithCount.count > 1;
+  
+-- Remove ALMOST identical rows. 
+-- We want to pick those with more recent "from_date".
+
+SELECT DISTINCT ON (emp_no, 
+					first_name, 
+					last_name)
+* FROM retiring_titles;
+
+-- From quick check above, there seems to be 33,118 distinct employees. 
+-- 33,118 unique employees, confirmed again below.
+
+
+SELECT emp_no, 
+	first_name, 
+	last_name, 
+	title, 
+	from_date, 
+	salary 
+INTO retiring_titles_unique
+FROM
+  (SELECT emp_no, 
+	first_name, 
+	last_name, 
+	title, 
+	from_date, 
+	salary,
+     ROW_NUMBER() OVER 
+(PARTITION BY (emp_no) 
+ ORDER BY from_date DESC) rn
+   FROM retiring_titles
+  ) tmp WHERE rn = 1
+ ORDER BY emp_no;
+ 
